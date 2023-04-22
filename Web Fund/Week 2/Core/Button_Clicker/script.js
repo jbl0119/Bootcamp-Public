78a4b94a7d57077161b579700a860074d2abf759
jbl0119 <127805788+jbl0119@users.noreply.github.com>
2023-04-21 20:41:23 -0400

    initial commit

---
+++ Web Fund/Week 2/Core/Button_Clicker/script.js
@@ -0,0 +1,16 @@
+function like_alert(){
+    alert("Ninja was liked")
+}
+
+function logout(element){
+    if (element.innerText === "Login"){
+        element.innerText = "Logout"
+} 
+    else {
+    element.innerText = "Login"
+    }
+}
+
+function hide(element) {
+    element.remove();
+}
